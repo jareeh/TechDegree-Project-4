5d4bd0bf6256fcdc6374e5925326ca1e92f110a5
Jeff Reeh <65681066+jareeh@users.noreply.github.com>
2020-09-18 14:13:40 -0500

    Built classes and constructors, enough methods to start game

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,50 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game{
+     constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases()
+        this.activePhrase = null;
+    }
+
+
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases(){
+        const phrase1 = new Phrase('Hi there');
+        const phrase2 = new Phrase('javacsript');
+        const phrase3 = new Phrase('I Love you');
+        const phrase4 = new Phrase('How are you');
+        const phrase5 = new Phrase('Austin Texas');
+        return [phrase1,
+                phrase2,
+                phrase3,
+                phrase4,
+                phrase5];
+    }
+
+
+    /**
+     * Selects random phrase from phrases property
+     * @return {Object} Phrase object chosen to be used
+     */
+    getRandomPhrase(){
+        const random = Math.floor(Math.random() * 5);
+        return this.phrases[random]
+    }
+
+
+    /**
+     * Begins game by selecting a random phrase and displaying it to user
+     */
+    startGame() {
+        const overlay = document.getElementById('overlay')
+        overlay.style.display = 'none';
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    };
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+     constructor(phrase){
+        this.phrase = phrase.toLowerCase();
+     }
+
+     /**
+     * Display phrase on game board
+     */
+     addPhraseToDisplay(){
+        const ul = document.querySelector('#phrase ul')
+        phrase = game.activePhrase;
+        console.log(phrase.phrase)
+
+        for (let i = 0; i < phrase.phrase.length; i++){
+            const li = document.createElement('li');
+
+            if(phrase.phrase.charAt(i) === ' '){
+                li.className = `space`;
+                ul.appendChild(li);
+            } else {
+                li.className = `hide letter ${phrase.phrase.charAt(i)}`;
+                li.textContent = `${phrase.phrase.charAt(i)}`;
+                ul.appendChild(li);
+            }
+        }
+    };
+
+    //  checkLetter();
+
+    //  showMatchedLetter();
+ } 
